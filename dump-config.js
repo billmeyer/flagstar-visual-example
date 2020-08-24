b00a5fdb255983ec64217f7b4f7c1d12291b5181
Bill Meyer <thebillmeyer@gmail.com>
2020-08-24 09:52:04 -0500

    Added dump-config capabilities

---
+++ dump-config.js
@@ -0,0 +1,4 @@
+console.log(`Node.js version: [${process.version}]`)
+console.log(`username: [${process.env.SAUCE_USERNAME}]`)
+console.log(`accessKey: [${process.env.SAUCE_ACCESS_KEY}]`)
+console.log(`apiKey: [${process.env.SCREENER_API_KEY}]`)
